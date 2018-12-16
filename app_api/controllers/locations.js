0a811d96b3c239b5fbe9b52d6839eea5ba8637eb
soanni <andrey.solodov@aurea.com>
2018-12-16 18:54:26 +0400

    calling api from the main app

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -69,7 +69,7 @@ const locationsListByDistance = async (req, res) => {
 				address: result.address,
 				rating: result.rating,
 				facilities: result.facilities,
-				distance: `${result.distance.calculated.toFixed()}m`
+				distance: `${result.distance.calculated.toFixed()}`
 			};
 		});
 		return res
--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,37 +1,101 @@
-/* GET 'home' page */
-const homelist = (req, res) => {
+const request = require('request');
+const apiOptions = {
+	server: 'http://localhost:3000'
+};
+
+const formatDistance = (distance) => {
+	let thisDistance = 0;
+	let unit = 'm';
+	if (distance > 1000) {
+		thisDistance = parseFloat(distance / 1000).toFixed(1);
+		unit = 'km';
+	} else {
+		thisDistance = Math.floor(distance);
+	}
+	return thisDistance + unit;
+};
+
+const renderHomepage = (req, res, responseBody) => {
+	let message = null;
+	if (!(responseBody instanceof Array)) {
+		message = "API lookup error";
+		responseBody = [];
+	} else {
+		if (!responseBody.length) {
+			message = "No places found nearby";
+		}
+	}
 	res.render('locations-list', {
 		title: 'Loc8r - find a place to work with wifi',
-        pageHeader: {
+        	pageHeader: {
 			title: 'Loc8r',
 			strapline: 'Find places to work with wifi near you!'
-        },
-        sidebar: 'Loc8r helps you to find places to work when out and about.',
-		locations: [{
-			name: 'Starcups',
-			address: '125 High Street, Reading, RG6 1PS',
-			rating: 3,
-			facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-			distance: '100m'
-		},{
-			name: 'Cafe Hero',
-			address: '125 High Street, Reading, RG6 1PS',
-			rating: 4,
-			facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-			distance: '200m'
-		},{
-			name: 'Burger Queen',
-			address: '125 High Street, Reading, RG6 1PS',
-			rating: 2,
-			facilities: ['Food', 'Premium wifi'],
-			distance: '250m'
-		}]
+        	},
+        	sidebar: 'Loc8r helps you to find places to work when out and about.',
+		locations: responseBody,
+		message
+	});
+};
+
+const renderDetailPage = (req, res, location) => {
+	res.render('location-info', {
+		title: location.name,
+		pageHeader: {
+			title: location.name
+		},
+		sidebar: {
+			context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+			callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+		},
+		location
+	});
+};
+		
+
+/* GET 'home' page */
+const homelist = (req, res) => {
+	const path = '/api/locations';
+	const requestOptions = {
+		url: `${apiOptions.server}${path}`,
+		method: 'GET',
+		json: {},
+		qs: {
+			lng: -0.9580780,
+			lat: 51.444130,
+			//lng: 0,
+			//lat: 0,
+			maxDistance: 20
+		}
+	};
+	request(requestOptions, (err, {statusCode}, body) => {
+		let data = [];
+		if (statusCode === 200 && body.length) {
+			data = body.map((item) => {
+				item.distance = formatDistance(item.distance);
+				return item;
+			});
+		}
+		renderHomepage(req, res, data);
 	});
 };
 
 /* GET 'Location info' page */
 const locationInfo = (req, res) => {
-	res.render('location-info', {title: 'Location Info'});
+	const path = `/api/locations/${req.params.locationid}`;
+	const requestOptions = {
+		url: `${apiOptions.server}${path}`,
+		method: 'GET',
+		json: {}
+	};
+	request(requestOptions, (err, response, body) => {
+		const data = body;
+		console.log(data);
+		data.coords = {
+			lng: body.coords[0],
+			lat: body.coords[1]
+		};
+		renderDetailPage(req, res, data); 
+	});
 };
 
 /* GET 'Add review' page */
@@ -39,8 +103,12 @@ const addReview = (req, res) => {
 	res.render('location-review-form', {title: 'Add review'});
 };
 
+const doAddReview = (req, res) => {
+};
+
 module.exports = {
 	homelist,
 	locationInfo,
-	addReview
+	addReview,
+	doAddReview
 };
